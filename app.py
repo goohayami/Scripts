d83669d9de637533042c06531587d4ef1ffdd47b
goohayami <goohayami@gmail.com>
2023-03-25 09:21:57 +0900

    fa

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('はじめての Streamlit')
+st.header('Streamlit')
+st.text('はじめてstreamlitを触ってみた。インストールからrunまでとても簡単で、Flaskのような')
+st.text('環境変数の設定も必要なくて、初心者の自分にも楽にwebサイトを作ることが出来ました！')
+
